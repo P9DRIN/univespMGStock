69c018d0fe0e6c7897f35ffa2982c062815be1af
Lima Silva <pedrohenriquelimasilva2837@hotmail.com>
2024-05-11 12:03:11 -0300

    fix: API URL

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export const api = axios.create({
-    // baseURL: 'https://api-univesp-mgs-tock.vercel.app'
-    baseURL: 'http://localhost:3333'
+    baseURL: 'https://api-univesp-mgs-tock.vercel.app'
+    // baseURL: 'http://localhost:3333'
 })
